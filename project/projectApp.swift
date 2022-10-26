8433dd7cc9f3fc256ee7f9440a8e504be55b5e5b
sumayah194 <saalrowaibaah22@twq.idserve.net>
2022-10-26 09:43:59 +0300

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by sumayah on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
